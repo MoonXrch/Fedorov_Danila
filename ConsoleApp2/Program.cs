7dfe16978f3561cc30e6b00fa4b14b219b690cbd
Fedor <danila210404@gmail.com>
2023-05-29 14:20:50 +0300

    Task2 Complete

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApp2;
+using System;
+namespace Program;
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Задания с условием");
+        Console.WriteLine();
+        Conditious conditious = new Conditious();
+        conditious.Task1();
+        conditious.Task2();
+        conditious.Task3();
+        conditious.Task4();
+        conditious.Task5();
+
+        Console.WriteLine();
+        Console.WriteLine("Задания с циклом");
+        Console.WriteLine();
+        Cycle cycle = new Cycle();
+        cycle.Task1();
+        cycle.Task2();
+        cycle.Task3();
+        cycle.Task4();
+    }
+
+    
+}
